d71f15be08dd1182b34e82a0533d841f57001600
declan <syas0301@gmail.com>
2022-05-31 21:55:21 +0900

    File Transfer Popup 창으로 변경

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const server = require("http").createServer(app);
 let port = process.env.PORT || 5000;
 
 app.set("view engine", "ejs");
-app.get("/fileTransfer/api/v1", (req, res) => {
+app.get("/api/v1/fileTransfer", (req, res) => {
     res.render("file");
 });
 
